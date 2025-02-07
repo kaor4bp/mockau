d77cdb012af13072a5c93bb80f494891d036fc47
Timofey Dolganov <tdolganov@gotrg.com>
2025-02-07 23:12:24 +0400

    Fix HttpRequestErrorEventModel event types

--- src/core/http/events/models/http_request_error_event_model.py
+++ src/core/http/events/models/http_request_error_event_model.py
@@ -1,7 +1,5 @@
-from typing import Literal
-
 from core.http.events.models.base_http_event_model import BaseHttpEventModel
 
 
 class HttpRequestErrorEventModel(BaseHttpEventModel):
-    event: Literal['http_request_too_many_redirects', 'http_request_actions_mismatched', 'http_request_no_action_found']
+    pass
