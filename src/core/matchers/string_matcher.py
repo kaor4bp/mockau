02c57bbba555c191698c36b95ac78fbd08b4f440
Timofey Rabbit <kaor.bp@gmail.com>
2025-02-13 23:38:36 +0300

    Return direct matching in Matchers

--- src/core/matchers/string_matcher.py
+++ src/core/matchers/string_matcher.py
@@ -1,3 +1,4 @@
+import re
 from typing import Annotated, Optional
 
 from pydantic import Field
@@ -90,7 +91,43 @@ class StringMatcher(SetVariableMatcher):
 
     @variables_context_transaction
     def is_matched(self, value, *, context: VariablesContext) -> bool:
-        return self.to_plain_matcher(context=context).is_matched(value)
+        if self.ignore_case:
+            value = value.lower()
+
+        if self.pattern is not None and not re.fullmatch(
+            self.pattern, value, flags=re.IGNORECASE if self.ignore_case else 0
+        ):
+            return False
+
+        if self.ignore_case:
+            if self.equal_to is not None and self.equal_to.lower() != value:
+                return False
+            if self.contains is not None and self.contains.lower() not in value:
+                return False
+            if self.startswith is not None and not value.lower().startswith(self.startswith.lower()):
+                return False
+            if self.endswith is not None and not value.lower().endswith(self.endswith.lower()):
+                return False
+        else:
+            if self.equal_to is not None and self.equal_to != value:
+                return False
+            if self.contains is not None and self.contains not in value:
+                return False
+            if self.startswith is not None and not value.startswith(self.startswith):
+                return False
+            if self.endswith is not None and not value.endswith(self.endswith):
+                return False
+
+        if self.and_ is not None and any(not item.is_matched(value, context=context) for item in self.and_):
+            return False
+        if self.or_ is not None and all(not item.is_matched(value, context=context) for item in self.or_):
+            return False
+        if self.not_ is not None and self.not_.is_matched(value, context=context):
+            return False
+        if self.set_variable is not None and not self.is_variable_matched(value, context=context):
+            return False
+
+        return True
 
 
 t_StringMatcher = Annotated[
